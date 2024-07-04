a291b1aa073798b7317db9f36184183e9102e59b
Carmin Vuong <carminvuong@gmail.com>
2024-07-04 19:01:32 -0400

    add interface

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 from utils import *
+from tkinter import *
 import csv
 
+r = Tk()
+r.title("Fifa Renderz Player Data")
+r.geometry("600x400")
 
-if __name__ == "__main__":
-    filename = input("Datafile name (without extension): ") + ".csv"
+# declaring string variable for url
+url_var=StringVar()
+
+def submit():
+    url = url_var.get()
+    url_var.set("")
+    print(url)
+
+
+url_label = Label(r, text='URL',font=('calibre',10, 'bold'))
+url_label.grid(row=0)
+url_entry = Entry(r, textvariable=url_var, font=('calibre',10,'normal'), width=40)
+url_entry.grid(row=0, column=1,padx=10)
+
+
+submit_button = Button(r, text='submit', width=5, command=submit)
+submit_button.grid(row=0, column=2)
+
+destroy_button = Button(r, text='close window', width=10, command=r.destroy)
+destroy_button.grid(row=1, column=1)
+
+r.mainloop()
+# filename = input("Datafile name (without extension): ") + ".csv"
+
+# with open(filename, 'a', encoding="utf-8", newline='') as file:
+    # w = csv.writer(file)
+    # url = input("Type in URL of player: ")
+    # all_stats = getStats(url) # stat values
+    # w.writerow(all_stats)
+
     
-    with open(filename, 'a', encoding="utf-8", newline='') as file:
-        w = csv.writer(file)
-        url = input("Type in URL of player: ")
-        all_stats = getStats(url) # stat values
-        w.writerow(all_stats)
 
     
--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
     randomURL = "https://renderz.app/24/player/30901310" # this is just for header (it doenst really matter who it is)
     header = getLabels(randomURL) # labels
 
-    filename = input("Datafile name (without extension): ") + ".csv"
-    
     # write the header first
-    with open(filename, 'w', encoding="utf-8", newline='') as file:
+    with open("data.csv", 'w', encoding="utf-8", newline='') as file:
         w = csv.writer(file)
         w.writerow(header)
